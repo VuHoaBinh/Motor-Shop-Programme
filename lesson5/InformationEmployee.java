5e726f42a479f89f97bbdb045afc2b85d4c24f23
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-09-25 10:47:22 +0700

    Add files via upload

--- lesson5/InformationEmployee.java
+++ lesson5/InformationEmployee.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.Box;
 import javax.swing.DefaultCellEditor;
@@ -21,33 +23,43 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-public class InformationEmployee extends JFrame implements ActionListener{
+public class InformationEmployee extends JFrame implements ActionListener, MouseListener{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	//label
 	private JLabel lbID;
-	private JTextField txtID;
 	private JLabel lbFirstName;
-	private JTextField txtFirstName;
 	private JLabel lbLastName;
-	private JTextField txtLastName;
 	private JLabel lbAge;
+	private JLabel lbSalary;
+	private JLabel lbNhap;
+	
+	//txt
+	private JTextField txtID;
+	private JTextField txtFirstName;
+	private JTextField txtLastName;
 	private JTextField txtAge;
+	private JTextField txtSalary;
+	private JTextField txtNhap;
+	
+	//rad
 	private JRadioButton radMen;
 	private JRadioButton radWomen;
-	private JLabel lbSalary;
-	private JTextField txtSalary;
+	private Nhanvien_collections listNV;
+	
+	
 	DefaultTableModel modelEmployee;
 	JTable tblEmployee;
-	private JLabel lbNhap;
-	private JTextField txtNhap;
+	private JTable btnGroup;
+
+	//btn
 	private JButton btnFind;
 	private JButton btnAdd;
 	private JButton btnDeleteSpace;
 	private JButton btnDelete;
 	private JButton btnSave;
-	private Nhanvien_collections listNV;
 	
 	InformationEmployee(){		
 		super();
@@ -113,6 +125,8 @@ public class InformationEmployee extends JFrame implements ActionListener{
 		
 		// center
 		String[] cols = {"Ma so", "Ho", "Ten nhan vien", "Tuoi", "Phai", "Tien luong"};
+		
+		//list table
 		modelEmployee = new DefaultTableModel(cols,0);
 		tblEmployee = new JTable(modelEmployee);
 		JScrollPane pane = new JScrollPane(tblEmployee);
@@ -142,14 +156,15 @@ public class InformationEmployee extends JFrame implements ActionListener{
 		jp_footer.add(btnDelete = new JButton("Xoa"));
 		jp_footer.add(btnSave = new JButton("Luu"));
 		
-//		lbNhap.addAncestorListener(this);
-//		btnFind.addAncestorListener(this);
+		
+		
+		btnFind.addActionListener(this);
 		btnAdd.addActionListener(this);
-//		btnDeleteSpace.addAncestorListener(this);
-//		btnDelete.addAncestorListener(this);
-//		lbNhap.addAncestorListener(this);
-//		btnSave.addAncestorListener(this);
+		btnDeleteSpace.addActionListener(this);
+		btnDelete.addActionListener(this);
+		btnSave.addActionListener(this);
 
+		tblEmployee.addMouseListener(this);
 		
 		
 		add(jp_footer, BorderLayout.SOUTH);
@@ -167,15 +182,17 @@ public class InformationEmployee extends JFrame implements ActionListener{
 			String FirstName = txtFirstName.getText();
 			String LastName = txtLastName.getText();
 			int Age = Integer.parseInt(txtAge.getText());
+			
 			System.out.println(Age);
 			System.out.println("===================");
+			
 			Boolean isSex = radWomen.isSelected();
 			String gender;
-	        if (isSex) {
-	            gender = "Nữ";
-	        } else {
-	            gender = "Nam";
-	        }
+			if (isSex) {
+				gender = "Nữ";
+			} else {
+				gender = "Nam";
+			}
 			double Salary = Double.parseDouble(txtSalary.getText());
 			NhanVien nv1 = new NhanVien(ID, FirstName, LastName, Age, isSex, Salary);
 			if(!listNV.themNhanVien(nv1)) {
@@ -184,8 +201,142 @@ public class InformationEmployee extends JFrame implements ActionListener{
 				modelEmployee.addRow(new Object[] {nv1.getEmployeeID(), nv1.getFirstName(), 
 						nv1.getLastName(), nv1.getAge(), gender, nv1.getSalary() +"$"} );
 			}
-			
 		}
 		
+		if (o == btnDeleteSpace) {
+			txtID.setText("");
+			txtID.requestFocus();
+			txtFirstName.setText("");
+			txtLastName.setText("");
+			txtAge.setText("");
+			txtSalary.setText("");
+			if(radMen.isSelected() || radWomen.isSelected() )
+            {
+				btnGroup.clearSelection();
+            }
+		}
+		
+		
+		if(o == btnFind) {
+			String valueID = txtID.getText();
+			if(valueID !=null && valueID.trim().length()>0) {
+				try {
+					NhanVien nv = listNV.timNhanVien(valueID);
+					if(nv != null) {
+						deleteAll();
+						String gender = nv.isSex() ? "Nam" : "Nu";
+						modelEmployee.addRow(new Object[] {nv.getEmployeeID(),
+								nv.getFirstName(), nv.getLastName(), nv.getAge(),
+								gender,nv.getSalary()});
+					}
+				}catch(Exception ex) {
+					JOptionPane.showMessageDialog(null, "Wrong");
+					txtID.selectAll();
+					txtID.requestFocus();
+				}
+			}else {
+				deleteAll();
+				ReadDataToList();
+				tblEmployee.setModel(modelEmployee);
+			}
+		}
+			
+		
+		if(o == btnDelete) {
+			int rowChoice = tblEmployee.getSelectedRow();
+			modelEmployee.removeRow(rowChoice);
+			NhanVien nv = listNV.getElement(rowChoice);
+			listNV.xoaNhanVien(nv.getEmployeeID());
+		}
+		
+		if(o == btnSave) {
+			// get row
+			int rowChoice = tblEmployee.getSelectedRow(); 
+
+	        String ID = txtID.getText();
+	        String FirstName = txtFirstName.getText();
+	        String LastName = txtLastName.getText();
+	        String Age = txtAge.getText();
+	        String gender = null;
+	        if (radMen.isSelected()) {
+	            gender = "Nam";
+		        radWomen.setSelected(false);
+	        } else {
+	            gender = "Nu";
+	            radMen.setSelected(false);
+	        }
+	        String Salary = txtSalary.getText();
+
+	        // Update 
+	        modelEmployee.setValueAt(ID, rowChoice, 0);
+	        modelEmployee.setValueAt(FirstName, rowChoice, 1);
+	        modelEmployee.setValueAt(LastName, rowChoice, 2);
+	        modelEmployee.setValueAt(Age, rowChoice, 3);
+	        modelEmployee.setValueAt(gender, rowChoice, 4);
+	        modelEmployee.setValueAt(Salary + "$", rowChoice, 5);
+		    
+		}
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		int rowSelection = tblEmployee.getSelectedRow();
+		txtID.setText(modelEmployee.getValueAt(rowSelection, 0).toString());
+		txtFirstName.setText(modelEmployee.getValueAt(rowSelection, 0).toString());
+		txtLastName.setText(modelEmployee.getValueAt(rowSelection, 0).toString());
+		txtAge.setText(modelEmployee.getValueAt(rowSelection, 0).toString());
+		String gender = modelEmployee.getValueAt(rowSelection, 4).toString();
+	    
+	    // Set the gender radio buttons 
+	    if (gender.equals("Nam")) {
+	        radMen.setSelected(true);
+	        radWomen.setSelected(false);
+	    } else if (gender.equals("Nu")) {
+	    	radMen.setSelected(false);
+	        radWomen.setSelected(true);
+	    }
+		
+		txtSalary.setText(modelEmployee.getValueAt(rowSelection, 0).toString());
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void ReadDataToList() {
+		for(int i=0;i<listNV.getSize();i++) {
+			NhanVien nv = listNV.getElement(i);
+			String gender = nv.isSex() ? "Nam" : "Nu";
+			modelEmployee.addRow(new Object[] {nv.getEmployeeID(),
+					nv.getFirstName(), nv.getLastName(), nv.getAge(),
+					gender,nv.getSalary()});
+		}
+	}
+	
+	public void deleteAll() {
+		DefaultTableModel tableModel = (DefaultTableModel) tblEmployee.getModel();
+		tableModel.getDataVector().removeAllElements();
 	}
 }
--- lesson5/Nhanvien_collections.java
+++ lesson5/Nhanvien_collections.java
@@ -21,4 +21,29 @@ public class Nhanvien_collections implements Serializable{
 		dsNV.add(nv);
 		return true;
 	}
+	public NhanVien getElement(int index) {
+		if(index <0 || index>=dsNV.size()) {
+			return null;
+		}
+		return dsNV.get(index);
+	}
+	public int getSize() {
+		return dsNV.size();
+	}
+	public boolean xoaNhanVien(String maNV) {
+		NhanVien nv = new NhanVien();
+		if(dsNV.contains(nv)) {
+			dsNV.remove(nv);
+			return true;
+		}
+		return false;
+	}
+	public NhanVien timNhanVien(String id) {
+        for (NhanVien nv : dsNV) {
+            if (nv.getEmployeeID().equals(id)) {
+                return nv;
+            }
+        }
+        return null; 
+    }
 }
