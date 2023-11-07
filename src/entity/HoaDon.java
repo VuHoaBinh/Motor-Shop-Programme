4ce9ca5d05e7e4b13c4d0946ba607d73e45dcee4
Boi <anhtuan03.dev@gmail.com>
2023-11-07 14:55:59 +0700

    update entity, dao

---
+++ src/entity/HoaDon.java
@@ -0,0 +1,98 @@
+package entity;
+
+import java.sql.Date;
+
+public class HoaDon {
+	private String maHoaDon;
+	private Date ngayLapHoaDon;
+	private int soLanTraGop;
+	private double tienNhanVao;
+	private double tienThua;
+	private NhanVien nhanVien;
+	private KhachHang khachHang;
+
+	public HoaDon() {
+		super();
+	}
+
+	public HoaDon(String maHoaDon) {
+		super();
+		this.maHoaDon = maHoaDon;
+	}
+
+	public HoaDon(String maHoaDon, Date ngayLapHoaDon, int soLanTraGop, double tienNhanVao, double tienThua,
+			NhanVien nhanVien, KhachHang khachHang) {
+		super();
+		this.maHoaDon = maHoaDon;
+		this.ngayLapHoaDon = ngayLapHoaDon;
+		this.soLanTraGop = soLanTraGop;
+		this.tienNhanVao = tienNhanVao;
+		this.tienThua = tienThua;
+		this.nhanVien = nhanVien;
+		this.khachHang = khachHang;
+	}
+
+	public String getMaHoaDon() {
+		return maHoaDon;
+	}
+
+	public void setMaHoaDon(String maHoaDon) {
+		this.maHoaDon = maHoaDon;
+	}
+
+	public Date getNgayLapHoaDon() {
+		return ngayLapHoaDon;
+	}
+
+	public void setNgayLapHoaDon(Date ngayLapHoaDon) {
+		this.ngayLapHoaDon = ngayLapHoaDon;
+	}
+
+	public int getSoLanTraGop() {
+		return soLanTraGop;
+	}
+
+	public void setSoLanTraGop(int soLanTraGop) {
+		this.soLanTraGop = soLanTraGop;
+	}
+
+	public double getTienNhanVao() {
+		return tienNhanVao;
+	}
+
+	public void setTienNhanVao(double tienNhanVao) {
+		this.tienNhanVao = tienNhanVao;
+	}
+
+	public double getTienThua() {
+		return tienThua;
+	}
+
+	public void setTienThua(double tienThua) {
+		this.tienThua = tienThua;
+	}
+
+	public NhanVien getNhanVien() {
+		return nhanVien;
+	}
+
+	public void setNhanVien(NhanVien nhanVien) {
+		this.nhanVien = nhanVien;
+	}
+
+	public KhachHang getKhachHang() {
+		return khachHang;
+	}
+
+	public void setKhachHang(KhachHang khachHang) {
+		this.khachHang = khachHang;
+	}
+
+	@Override
+	public String toString() {
+		return "HoaDon [maHoaDon=" + maHoaDon + ", ngayLapHoaDon=" + ngayLapHoaDon + ", soLanTraGop=" + soLanTraGop
+				+ ", tienNhanVao=" + tienNhanVao + ", tienThua=" + tienThua + ", nhanVien=" + nhanVien + ", khachHang="
+				+ khachHang + "]";
+	}
+
+}
---
+++ src/entity/NhaPhanPhoi.java
@@ -0,0 +1,59 @@
+package entity;
+
+public class NhaPhanPhoi {
+	private String maNhaPhanPhoi;
+	private String tenNhapPhanPhoi;
+	private String sdt;
+	private String diaChi;
+
+	public NhaPhanPhoi() {
+		super();
+	}
+
+	public NhaPhanPhoi(String maNhaPhanPhoi, String tenNhapPhanPhoi, String sdt, String diaChi) {
+		super();
+		this.maNhaPhanPhoi = maNhaPhanPhoi;
+		this.tenNhapPhanPhoi = tenNhapPhanPhoi;
+		this.sdt = sdt;
+		this.diaChi = diaChi;
+	}
+
+	public String getMaNhaPhanPhoi() {
+		return maNhaPhanPhoi;
+	}
+
+	public void setMaNhaPhanPhoi(String maNhaPhanPhoi) {
+		this.maNhaPhanPhoi = maNhaPhanPhoi;
+	}
+
+	public String getTenNhapPhanPhoi() {
+		return tenNhapPhanPhoi;
+	}
+
+	public void setTenNhapPhanPhoi(String tenNhapPhanPhoi) {
+		this.tenNhapPhanPhoi = tenNhapPhanPhoi;
+	}
+
+	public String getSdt() {
+		return sdt;
+	}
+
+	public void setSdt(String sdt) {
+		this.sdt = sdt;
+	}
+
+	public String getDiaChi() {
+		return diaChi;
+	}
+
+	public void setDiaChi(String diaChi) {
+		this.diaChi = diaChi;
+	}
+
+	@Override
+	public String toString() {
+		return "NhaPhanPhoi [maNhaPhanPhoi=" + maNhaPhanPhoi + ", tenNhapPhanPhoi=" + tenNhapPhanPhoi + ", sdt=" + sdt
+				+ ", diaChi=" + diaChi + "]";
+	}
+
+}
